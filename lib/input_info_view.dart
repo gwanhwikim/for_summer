1d33a3e435f5e6ff92c3f2fdda54e068a566df1c
gwanhwikim <rhksgnl11@naver.com>
2023-05-15 13:35:00 +0900

    first commit

---
+++ lib/input_info_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class InputInfoView extends StatefulWidget {
+  const InputInfoView({Key? key}) : super(key: key);
+
+  @override
+  State<InputInfoView> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<InputInfoView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: Column(),
+    );
+  }
+}
